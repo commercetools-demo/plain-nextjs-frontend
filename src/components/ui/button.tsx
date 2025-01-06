47e0c776f4f9748fb20af34105e03ecdcf34bb61
Behnam Tehrani <behnam777@gmail.com>
2025-01-06 13:23:04 -0500

    button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,10 +9,10 @@ type Props = HTMLAttributes<HTMLButtonElement> & {
 export const Button = ({ loading, ...props }: Props) => {
     return (
         <button {...props} className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
-            <>
+            <div className="inline">
             {props.children}
             {loading && <LoadingSpinnerComponent />}
-            </>
+            </div>
         </button>
     )
 }
